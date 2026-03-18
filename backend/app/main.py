42087f7b9aa91d2fa71eadf6c7bc3591789e7172
ACERCSI\2202103 <euler.hsing@acercsi.com>
2026-03-18 15:12:38 +0800

    [Fix] fix backend cross

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import os
+from fastapi.middleware.cors import CORSMiddleware
 import psycopg2
 from app.api.documents import router as documents_router
 from app.api.search import router as search_router
@@ -7,6 +8,17 @@ from app.api.qa import router as qa_router
 
 # FastAPI 應用入口
 app = FastAPI(title="RAG Docs System")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://10.16.43.66:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/health")
